1ec6cad48af5d9a9dc67a184c26813a08f3c0eea
davidsproject <davidgarciasaragih6@gmail.com>
2023-02-09 00:26:23 +0700

    done

--- resources/views/layouts/backend/admin/master.blade.php
+++
@@ -1,6 +0,0 @@
-@include('layouts.backend.admin.sidebar')
-@include('layouts.backend.admin.navbar')
-  <div class="container-fluid">
-    @yield('content')
-  </div>
-@include('layouts.backend.admin.footer')
